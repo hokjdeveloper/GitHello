58bf8620ed6c5e59c2ae4bd7c62fa0476b75323e
hok <hok@hoks-MacBook-Pro.local>
2015-09-03 11:46:45 +0800

    hello,init

---
+++ hello/hello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hello
+//
+//  Created by hok on 9/3/15.
+//  Copyright (c) 2015 hok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
